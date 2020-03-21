01c1518259e4031b355e78791ab90c811b8a2b81
Bill7zz <billytagzz@gmail.com>
2020-03-20 21:03:58 -0300

    Add SimpleConversationIdFactory class

--- SkillsFunctionalTests/python/host/skill_http_client.py
+++ SkillsFunctionalTests/python/host/skill_http_client.py
@@ -6,20 +6,54 @@ from botbuilder.core import (
     TurnContext,
 )
 
+from typing import Union
+
 from botbuilder.integration.aiohttp import BotFrameworkHttpClient
 from botbuilder.core.skills import (
     ConversationIdFactoryBase,
+    SkillConversationIdFactoryOptions,
+    SkillConversationReference,
     BotFrameworkSkill,
 )
-from botbuilder.schema import Activity
+from botbuilder.schema import Activity, ActivityTypes, ConversationReference
 from botframework.connector.auth import SimpleCredentialProvider
 
+class SimpleConversationIdFactory(ConversationIdFactoryBase):
+    def __init__(self):
+        self.conversation_refs = {}
+
+    async def create_skill_conversation_id(
+        self,
+        options_or_conversation_reference: Union[
+            SkillConversationIdFactoryOptions, ConversationReference
+        ],
+    ) -> str:
+        key = (
+            options_or_conversation_reference.activity.conversation.id
+            + options_or_conversation_reference.activity.service_url
+        )
+        if key not in self.conversation_refs:
+            self.conversation_refs[key] = SkillConversationReference(
+                conversation_reference=TurnContext.get_conversation_reference(
+                    options_or_conversation_reference.activity
+                ),
+                oauth_scope=options_or_conversation_reference.from_bot_oauth_scope,
+            )
+        return key
+
+    async def get_conversation_reference(
+        self, skill_conversation_id: str
+    ) -> Union[SkillConversationReference, ConversationReference]:
+        return self.conversation_refs[skill_conversation_id]
+
+    async def delete_conversation_reference(self, skill_conversation_id: str):
+        raise NotImplementedError()
 
 class SkillHttpClient(BotFrameworkHttpClient):
     def __init__(
         self,
         credential_provider: SimpleCredentialProvider,
-        skill_conversation_id_factory: ConversationIdFactoryBase,
+        skill_conversation_id_factory: SimpleConversationIdFactory,
     ):
         if not skill_conversation_id_factory:
             raise TypeError("skill_conversation_id_factory can't be None")
