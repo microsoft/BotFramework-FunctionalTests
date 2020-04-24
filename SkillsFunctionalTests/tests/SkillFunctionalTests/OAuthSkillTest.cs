1c2cb0a80f8cdcbc1e0c55b2d2781da0130d976c
Carlos Castro <ccastro@microsoft.com>
2020-04-23 22:31:12 -0700

    Tests: Improve test consistency, add new end of conversation test and move to Testbotclient

--- SkillsFunctionalTests/tests/SkillFunctionalTests/OAuthSkillTest.cs
+++ SkillsFunctionalTests/tests/SkillFunctionalTests/OAuthSkillTest.cs
@@ -18,21 +18,21 @@ namespace FunctionalTests
     public class OAuthSkillTest
     {
         [TestMethod]
-        public async Task OAuthCalledInSkill()
+        public async Task Skill_OAuthCard_SignInSuccessful()
         {
             // If the test takes more than one minute, declare failure.
             var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
 
-            var botChecker = new TestBotClient(new EnvironmentBotTestConfiguration());
+            var testBot = new TestBotClient(new EnvironmentBotTestConfiguration());
 
-            await botChecker.StartConversation(cancellationTokenSource.Token);
-            await botChecker.SendMessageAsync("Hello", cancellationTokenSource.Token);
-            await botChecker.AssertReplyAsync("Me no nothin", cancellationTokenSource.Token);
-            await botChecker.SendMessageAsync("skill", cancellationTokenSource.Token);
-            await botChecker.AssertReplyAsync("Echo: skill", cancellationTokenSource.Token);
-            await botChecker.SendMessageAsync("auth", cancellationTokenSource.Token);
-            var messages = await botChecker.ReadBotMessagesAsync(cancellationTokenSource.Token);
-            await botChecker.SignInAndVerifyOAuthAsync(messages.FirstOrDefault(m => m.Attachments != null && m.Attachments.Any()), cancellationTokenSource.Token);
+            await testBot.StartConversation(cancellationTokenSource.Token);
+            await testBot.SendMessageAsync("Hello", cancellationTokenSource.Token);
+            await testBot.AssertReplyAsync("Me no nothin", cancellationTokenSource.Token);
+            await testBot.SendMessageAsync("skill", cancellationTokenSource.Token);
+            await testBot.AssertReplyAsync("Echo: skill", cancellationTokenSource.Token);
+            await testBot.SendMessageAsync("auth", cancellationTokenSource.Token);
+            var messages = await testBot.ReadBotMessagesAsync(cancellationTokenSource.Token);
+            await testBot.SignInAndVerifyOAuthAsync(messages.FirstOrDefault(m => m.Attachments != null && m.Attachments.Any()), cancellationTokenSource.Token);
         }
     }
 }
