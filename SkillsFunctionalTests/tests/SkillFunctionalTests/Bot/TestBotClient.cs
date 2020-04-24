625d90b30f75ffa2596ac896364e7949c1f92b4c
Carlos Castro <ccastro@microsoft.com>
2020-04-23 23:11:20 -0700

    Tests: Add copyright header to all functional test classes

--- SkillsFunctionalTests/tests/SkillFunctionalTests/Bot/TestBotClient.cs
+++ SkillsFunctionalTests/tests/SkillFunctionalTests/Bot/TestBotClient.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Bot.Connector.DirectLine;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Connector.DirectLine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using SkillFunctionalTests.Configuration;
