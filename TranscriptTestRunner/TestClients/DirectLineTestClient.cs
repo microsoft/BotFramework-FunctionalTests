06765ad24c262d68afa6f1ddca2d398af4a6a842
Gabo Gilabert <ggilaber@microsoft.com>
2020-10-20 00:53:03 -0400

    Refactor some classes and optimizations - Updated SkillFunctionalTests.csproj to always copy anything under the SourceTranscripts folder so I don't forget to set the copy always property. - Changed TranscriptTestRunner.csproj to target netstandard2.0 and set it to use the latest C# version to make it more compatible with other consumers. - TestClientBase: Removed ValidateActivityAsync (this has been moved to the runner. - TestClientBase: Added GetNextReplyAsync to be able to pop activities from the client's internal queue. - DirectLineTestClient: made it disposabled and implemented the dispose pattern to ensure we dispose direct line client. - DirectLineTestClient: Implemented GetNextReplyAsync that stores whatever it pulls from DL in an internal queue and returns the first activity if any. - DirectLineTestClient: added watermark field to store latest read activity (as the https://github.com/microsoft/BotFramework-DirectLine-DotNet/tree/main/samples/core-DirectLine/DirectLineClient shows). - DirectLineTestClient: removed the validation code (this is now in TestRunner). - TestRunner: Added SendActivityAsync and GetNextReplyAsync so we can send an receive activities without a script if we want. - TestRunner: Implemented rough AssertReply method with logic to check activities. - TestRunner: implemented some very basic filtering.

--- TranscriptTestRunner/TestClients/DirectLineTestClient.cs
+++ TranscriptTestRunner/TestClients/DirectLineTestClient.cs
@@ -2,35 +2,49 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector.DirectLine;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using BotActivity = Microsoft.Bot.Schema.Activity;
 
 namespace TranscriptTestRunner.TestClients
 {
-    public class DirectLineTestClient : TestClientBase
+    public class DirectLineTestClient : TestClientBase, IDisposable
     {
+        // DL client sample: https://github.com/microsoft/BotFramework-DirectLine-DotNet/tree/main/samples/core-DirectLine/DirectLineClient
         private const string DirectLineSecretKey = "DIRECTLINE";
         private const string BotIdKey = "BOTID";
-        private static string _directLineSecret;
         private static string _botId;
+        private readonly ConcurrentQueue<BotActivity> _activityQueue = new ConcurrentQueue<BotActivity>();
+        private readonly DirectLineClient _dlClient;
         private readonly string _user = $"TestUser-{Guid.NewGuid()}";
         private string _conversationId;
-        
+
+        // To detect redundant calls to dispose
+        private bool _disposed;
+        private string _watermark;
+
         public DirectLineTestClient(IConfiguration config)
         {
-            GetConfiguration(config);
+            _botId = config[BotIdKey];
+            if (string.IsNullOrWhiteSpace(_botId))
+            {
+                throw new ArgumentException($"Configuration setting '{BotIdKey}' not set.");
+            }
 
-            Client = new DirectLineClient(_directLineSecret);
+            var directLineSecret = config[DirectLineSecretKey];
+            if (string.IsNullOrWhiteSpace(directLineSecret))
+            {
+                throw new ArgumentException($"Configuration setting '{DirectLineSecretKey}' not found.");
+            }
+
+            _dlClient = new DirectLineClient(directLineSecret);
         }
-        
-        private DirectLineClient Client { get; }
 
-        public override async Task SendActivityAsync(BotActivity activity)
+        public override async Task SendActivityAsync(BotActivity activity, CancellationToken cancellationToken)
         {
             if (string.IsNullOrWhiteSpace(_conversationId))
             {
@@ -44,66 +58,70 @@ namespace TranscriptTestRunner.TestClients
                 Type = activity.Type
             };
 
-            await Client.Conversations.PostActivityAsync(_conversationId, activityPost, default).ConfigureAwait(false);
+            await _dlClient.Conversations.PostActivityAsync(_conversationId, activityPost, cancellationToken).ConfigureAwait(false);
         }
 
-        public override async Task<bool> ValidateActivityAsync(BotActivity expected)
+        public override async Task<BotActivity> GetNextReplyAsync(CancellationToken cancellationToken)
         {
-            var activities = await PollBotMessagesAsync(default).ConfigureAwait(false);
-            var botMessages = activities.Where(a => a.Type == ActivityTypes.Message);
+            await PullActivitiesFromDirectLineAsync(cancellationToken).ConfigureAwait(false);
 
-            return botMessages.Any(message => message.Text == expected.Text);
-        }
-
-        private static void GetConfiguration(IConfiguration config)
-        {
-            _directLineSecret = config[DirectLineSecretKey];
-
-            if (string.IsNullOrWhiteSpace(_directLineSecret))
+            // Return the first activity in the queue (if any)
+            if (_activityQueue.TryDequeue(out var activity))
             {
-                throw new ArgumentException($"Configuration setting '{DirectLineSecretKey}' not found.");
+                return activity;
             }
 
-            _botId = config[BotIdKey];
-            
-            if (string.IsNullOrWhiteSpace(_botId))
-            {
-                throw new ArgumentException($"Configuration setting '{BotIdKey}' not set.");
-            }
+            // No activities in the queue
+            return null;
         }
 
-        private async Task<IEnumerable<Activity>> PollBotMessagesAsync(CancellationToken cancellationToken = default)
+        public void Dispose()
         {
-            using var maxCancellation = new CancellationTokenSource(TimeSpan.FromMinutes(2));
-
-            while (!cancellationToken.IsCancellationRequested && !maxCancellation.IsCancellationRequested)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken).ConfigureAwait(false);
-
-                var activities = await ReadBotMessagesAsync(cancellationToken).ConfigureAwait(false);
-
-                if (activities != null && activities.Any())
-                {
-                    return activities;
-                }
-            }
-
-            throw new Exception("No activities received");
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        private async Task<IEnumerable<Activity>> ReadBotMessagesAsync(CancellationToken cancellationToken = default)
+        protected virtual void Dispose(bool disposing)
         {
-            var activitySet = await Client.Conversations.GetActivitiesAsync(_conversationId, null, cancellationToken).ConfigureAwait(false);
+            if (_disposed)
+            {
+                return;
+            }
 
-            // Extract and return the activities sent from the bot.
-            return activitySet?.Activities?.Where(activity => activity.From.Id == _botId);
+            if (disposing)
+            {
+                // Dispose managed objects owned by the class here.
+                _dlClient?.Dispose();
+            }
+
+            _disposed = true;
         }
 
         private async Task CreateConversationAsync()
         {
-            var conversation = await Client.Conversations.StartConversationAsync().ConfigureAwait(false);
-
+            var conversation = await _dlClient.Conversations.StartConversationAsync().ConfigureAwait(false);
             _conversationId = conversation.ConversationId;
+        }
+
+        private async Task PullActivitiesFromDirectLineAsync(CancellationToken cancellationToken)
+        {
+            // Pull all the available activities from direct line
+            var activitySet = await _dlClient.Conversations.GetActivitiesAsync(_conversationId, _watermark, cancellationToken).ConfigureAwait(false);
+
+            if (activitySet != null)
+            {
+                _watermark = activitySet.Watermark;
+
+                // Add activities to the queue
+                foreach (var dlActivity in activitySet.Activities)
+                {
+                    if (dlActivity.From.Id == _botId)
+                    {
+                        // Convert the DL Activity object to a BF activity object.
+                        _activityQueue.Enqueue(JsonConvert.DeserializeObject<BotActivity>(JsonConvert.SerializeObject(dlActivity)));
+                    }
+                }
+            }
         }
     }
 }
--- TranscriptTestRunner/TestRunner.cs
+++ TranscriptTestRunner/TestRunner.cs
@@ -2,39 +2,78 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
+using Activity = Microsoft.Bot.Schema.Activity;
 
 namespace TranscriptTestRunner
 {
     public class TestRunner
     {
+        private readonly int _replyTimeout;
+        private readonly TestClientBase _testClient;
+
         public TestRunner(TestClientBase client)
         {
-            TestClientBase = client;
+            _testClient = client;
+            _replyTimeout = 45000;
         }
-
-        private TestClientBase TestClientBase { get; set; }
 
         private TranscriptConverter TranscriptConverter { get; set; }
 
+        // TODO: Not sure if it is better to avoid this and have another constructor.
         public static async Task RunTestAsync(ClientType client, params string[] transcriptPaths)
         {
             foreach (var transcriptPath in transcriptPaths)
             {
+                // TODO: This should be outside of the loop
                 var runner = new TestRunner(new TestClientFactory(client).GetTestClient());
-
                 await runner.RunTestAsync(transcriptPath).ConfigureAwait(false);
             }
         }
 
-        public async Task RunTestAsync(string transcriptPath)
+        public async Task RunTestAsync(string transcriptPath, CancellationToken cancellationToken = default)
         {
             ConvertTranscript(transcriptPath);
+            await ExecuteTestScriptAsync(cancellationToken).ConfigureAwait(false);
+        }
 
-            await ExecuteTestScriptAsync().ConfigureAwait(false);
+        public async Task SendActivityAsync(Activity sendActivity, CancellationToken cancellationToken)
+        {
+            await _testClient.SendActivityAsync(sendActivity, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<Activity> GetNextReplyAsync(CancellationToken cancellationToken)
+        {
+            var timeoutCheck = new Stopwatch();
+            timeoutCheck.Start();
+            while (true)
+            {
+                var activity = await _testClient.GetNextReplyAsync(cancellationToken).ConfigureAwait(false);
+                do
+                {
+                    if (activity != null && activity.Type != ActivityTypes.Trace && activity.Type != ActivityTypes.Typing)
+                    {
+                        return activity;
+                    }
+
+                    // Pop the next activity un the queue.
+                    activity = await _testClient.GetNextReplyAsync(cancellationToken).ConfigureAwait(false);
+                } 
+                while (activity != null);
+
+                // Wait a bit for the bot
+                await Task.Delay(TimeSpan.FromMilliseconds(50), cancellationToken).ConfigureAwait(false);
+
+                if (timeoutCheck.ElapsedMilliseconds > _replyTimeout)
+                {
+                    throw new TimeoutException("operation timed out while waiting for a response from the bot");
+                }
+            }
         }
 
         private void ConvertTranscript(string transcriptPath)
@@ -48,38 +87,62 @@ namespace TranscriptTestRunner
             TranscriptConverter.Convert();
         }
 
-        private async Task ExecuteTestScriptAsync()
+        private async Task ExecuteTestScriptAsync(CancellationToken cancellationToken)
         {
             using var reader = new StreamReader(TranscriptConverter.TestScript);
 
-            var testScript = JsonConvert.DeserializeObject<TestScript[]>(reader.ReadToEnd());
+            var testScript = JsonConvert.DeserializeObject<TestScript[]>(await reader.ReadToEndAsync().ConfigureAwait(false));
 
-            foreach (var script in testScript)
+            foreach (var scriptActivity in testScript)
             {
-               if (script.Role == "bot")
-               {
-                   var activity = new Activity
-                   {
-                       Type = script.Type,
-                       Text = script.Text
-                   };
+                switch (scriptActivity.Role)
+                {
+                    case RoleTypes.User:
+                        // Send the activity.
+                        var sendActivity = new Activity
+                        {
+                            Type = scriptActivity.Type,
+                            Text = scriptActivity.Text
+                        };
 
-                   if (!await TestClientBase.ValidateActivityAsync(activity).ConfigureAwait(false))
-                   {
-                       throw new Exception($"The bot didn't reply as expected. It should have said: {activity.Text}");
-                   }
-               }
-               else
-               {
-                   var activity = new Activity
-                   {
-                       Type = script.Type,
-                       Text = script.Text
-                   };
+                        await SendActivityAsync(sendActivity, cancellationToken).ConfigureAwait(false);
+                        break;
 
-                   await TestClientBase.SendActivityAsync(activity).ConfigureAwait(false);
-               }
+                    case RoleTypes.Bot:
+                        // Assert the activity returned
+                        if (!IgnoreScriptActivity(scriptActivity))
+                        {
+                            var activity = await GetNextReplyAsync(cancellationToken).ConfigureAwait(false);
+
+                            // Assert here
+                            AssertReply(scriptActivity, activity);
+                        }
+
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Invalid script activity type {scriptActivity.Role}.");
+                }
             }
+        }
+
+        // (temp approach)
+        private void AssertReply(TestScript expected, Activity actual)
+        {
+            if (expected.Type != actual.Type)
+            {
+                throw new Exception($"Invalid activity type. Expected: {expected.Type} Actual: {actual.Type}");
+            }
+
+            if (expected.Text != actual.Text)
+            {
+                throw new Exception($"Invalid activity text. Expected: {expected.Text} Actual: {actual.Text}");
+            }
+        }
+
+        private bool IgnoreScriptActivity(TestScript activity)
+        {
+            return activity.Type == ActivityTypes.Trace || activity.Type == ActivityTypes.Typing;
         }
     }
 }
