0a83d0b4dd74a41258ba8bbcf6f759e433a04ade
CeciliaAvila <cecilia.avila@southworks.com>
2020-11-03 16:49:03 -0300

    Set InputHint property in v3 bots

--- Bots/DotNet/Skills/CodeFirst/EchoSkillBot-v3/Dialogs/RootDialog.cs
+++ Bots/DotNet/Skills/CodeFirst/EchoSkillBot-v3/Dialogs/RootDialog.cs
@@ -22,19 +22,25 @@ namespace Microsoft.BotFrameworkFunctionalTests.EchoSkillBot.Dialogs
         {
             var activity = await result as Activity;
 
+            var options = new MessageOptions
+            {
+                InputHint = InputHints.AcceptingInput
+            };
+
             // Send an `endOfconversation` activity if the user cancels the skill.
             if (activity.Text.ToLower().Contains("end") || activity.Text.ToLower().Contains("stop"))
             {
-                await context.PostAsync($"Ending conversation from the skill...");
+                await context.SayAsync($"Ending conversation from the skill...", options: options);
                 var endOfConversation = activity.CreateReply();
                 endOfConversation.Type = ActivityTypes.EndOfConversation;
                 endOfConversation.Code = EndOfConversationCodes.CompletedSuccessfully;
+                endOfConversation.InputHint = InputHints.AcceptingInput;
                 await context.PostAsync(endOfConversation);
             }
             else
             {
-                await context.PostAsync($"Echo: {activity.Text}");
-                await context.PostAsync($"Say \"end\" or \"stop\" and I'll end the conversation and back to the parent.");
+                await context.SayAsync($"Echo: {activity.Text}", options: options);
+                await context.SayAsync($"Say \"end\" or \"stop\" and I'll end the conversation and back to the parent.", options: options);
             }
 
             context.Wait(MessageReceivedAsync);
