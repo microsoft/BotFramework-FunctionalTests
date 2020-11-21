95ecc6956c75556d0cead5eb58d731763c0a9cc6
Gabo Gilabert <ggilaber@microsoft.com>
2020-11-21 18:03:57 -0500

    Updated powershell to create local copies of composer files to make it more generic. Moved gitignore to exclude ComposerDialogs deployment folder to a shared location. Set isSkill to true in the composer echo skill settings;.

--- Bots/DotNet/Skills/Composer/EchoComposerSkillBot/runtime/azurewebapp/ArrangeComposer.ps1
+++ Bots/DotNet/Skills/Composer/EchoComposerSkillBot/runtime/azurewebapp/ArrangeComposer.ps1
@@ -1,24 +1,58 @@
-// This script arranges the composer files into the runtime project
-// so we can do a webdeploy of the project from azure
+# This script arranges the composer files into the runtime project
+# so we can do a webdeploy of the project from azure
 
-Remove-Item "ComposerDialogs" -Recurse
+# Helper to copy folders
+function Copy-Folder ($folderName)
+{
+    $sourcePath = "../../" +  $folderName
+    $targetPath = "ComposerDialogs/" + $folderName
+    if ( Test-Path -Path $sourcePath -PathType Container )
+    {
+        New-Item -Path $targetPath -ItemType Directory
+        copy-item ($sourcePath + "/*") $targetPath -force -recurse 
+    }
+}
 
-New-Item -Path "ComposerDialogs/dialogs" -ItemType Directory
-copy-item "../../dialogs/*" "ComposerDialogs/dialogs" -force -recurse -verbose
+# Helper to delete a folder (if it exists)
+function Delete-Folder ($folderName)
+{
+    if ( Test-Path -Path $folderName -PathType Container )
+    {
+        # Ensure bin folder is clean
+        Remove-Item $folderName -Recurse
+    }
+}
 
-New-Item -Path "ComposerDialogs/knowledge-base" -ItemType Directory
-copy-item "../../knowledge-base/*" "ComposerDialogs/knowledge-base" -force -recurse -verbose
 
-New-Item -Path "ComposerDialogs/language-generation" -ItemType Directory
-copy-item "../../language-generation/*" "ComposerDialogs/language-generation" -force -recurse -verbose
+# Cleanup
+Delete-Folder "bin"
+Delete-Folder "ComposerDialogs"
+Delete-Folder "wwwroot/manifests"
 
-New-Item -Path "ComposerDialogs/language-understanding" -ItemType Directory
-copy-item "../../language-understanding/*" "ComposerDialogs/language-understanding" -force -recurse -verbose
+# Create folder for composer files
+New-Item -Path "ComposerDialogs" -ItemType Directory
 
+# Copy composer folders
+Copy-Folder "dialogs"
+Copy-Folder "generated"
+Copy-Folder "knowledge-base"
+Copy-Folder "language-generation"
+Copy-Folder "language-understanding"
+Copy-Folder "recognizers"
+Copy-Folder "settings"
+
+# Copy schemas
 New-Item -Path "ComposerDialogs/schemas" -ItemType Directory
-copy-item "../../schemas/**/*.schema" "ComposerDialogs/schemas" -force -recurse -verbose
+copy-item "../../schemas/*.schema" "ComposerDialogs/schemas" -force -recurse 
 
-New-Item -Path "ComposerDialogs" -name "settings" -ItemType "directory"
-copy-item "../../settings/*" "ComposerDialogs/settings" -force -recurse -verbose
+# Copy root dialog
+copy-item "../../*.dialog" "ComposerDialogs/" -force -recurse 
 
-copy-item "../../*.dialog" "ComposerDialogs/" -force -recurse -verbose 
+# Copy manifests (if any)
+$sourcePath = "../../manifests"
+$targetPath = "wwwroot/manifests"
+if ( Test-Path -Path $sourcePath -PathType Container )
+{
+    New-Item -Path $targetPath -ItemType Directory
+    copy-item ($sourcePath + "/*") $targetPath -force -recurse 
+}
