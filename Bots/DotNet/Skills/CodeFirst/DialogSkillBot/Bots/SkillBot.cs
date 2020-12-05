29cf1d68c856bf5ee64ede0063d7905e69861984
virtual-josh <virtual-josh@server.no>
2020-12-04 17:09:54 -0800

    adding card dialog

--- Bots/DotNet/Skills/CodeFirst/DialogSkillBot/Bots/SkillBot.cs
+++ Bots/DotNet/Skills/CodeFirst/DialogSkillBot/Bots/SkillBot.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
 
 namespace Microsoft.BotFrameworkFunctionalTests.DialogSkillBot.Bots
 {
@@ -13,6 +18,7 @@ namespace Microsoft.BotFrameworkFunctionalTests.DialogSkillBot.Bots
     {
         private readonly ConversationState _conversationState;
         private readonly Dialog _mainDialog;
+        private readonly IHttpClientFactory _clientFactory;
 
         public SkillBot(ConversationState conversationState, T mainDialog)
         {
@@ -20,12 +26,42 @@ namespace Microsoft.BotFrameworkFunctionalTests.DialogSkillBot.Bots
             _mainDialog = mainDialog;
         }
 
+        public IHttpClientFactory GetClientFactory()
+        {
+            return _clientFactory;
+        }
+
         public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
         {
-            await _mainDialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+            if (turnContext.Activity.Type != ActivityTypes.ConversationUpdate)
+            {
+                // Run the Dialog with the Activity.
+                await _mainDialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+            }
+            else
+            {
+                // Let the base class handle the activity.
+                await base.OnTurnAsync(turnContext, cancellationToken);
+            }
 
             // Save any state changes that might have occurred during the turn.
             await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    var activity = MessageFactory.Text("Welcome to the dialog skill bot");
+                    activity.Speak = "Welcome to the Dialog Skill Prototype!";
+                    await turnContext.SendActivityAsync(activity, cancellationToken);
+                    await _mainDialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+                }
+            }
         }
     }
 }
