fe704cad60a0392f25ed4a9f7d9a866c4b31cd31
Gabo Gilabert <ggilaber@microsoft.com>
2021-01-26 10:50:13 -0500

    Refactored ProactiveMessages Modified proactive messages so the messages are sent only to the user that requested it rather than broadcasting messages to any user that has used the bot. Updated related test.

--- Bots/DotNet/Skills/CodeFirst/WaterfallSkillBot/Dialogs/Proactive/WaitForProactiveDialog.cs
+++ Bots/DotNet/Skills/CodeFirst/WaterfallSkillBot/Dialogs/Proactive/WaitForProactiveDialog.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -14,25 +17,34 @@ namespace Microsoft.BotFrameworkFunctionalTests.WaterfallSkillBot.Dialogs.Proact
     public class WaitForProactiveDialog : Dialog
     {
         // Message to send to users when the bot receives a Conversation Update event
-        private const string NotifyMessage = "Navigate to {0}api/notify?message={1} to proactively message everyone who has previously messaged this bot.";
+        private const string NotifyMessage = "Navigate to {0}api/notify?user={1} to proactively message the user.";
 
         private readonly Uri _serverUrl;
-        
-        public WaitForProactiveDialog(IHttpContextAccessor httpContextAccessor)
+        private readonly ConcurrentDictionary<string, ContinuationParameters> _continuationParametersStore;
+
+        public WaitForProactiveDialog(IHttpContextAccessor httpContextAccessor, ConcurrentDictionary<string, ContinuationParameters> continuationParametersStore)
         {
+            _continuationParametersStore = continuationParametersStore;
             _serverUrl = new Uri($"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host.Value}");
         }
 
-        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = new CancellationToken())
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
         {
-            await dc.Context.SendActivityAsync(MessageFactory.Text(string.Format(NotifyMessage, _serverUrl, Guid.NewGuid())), cancellationToken);
+            // Store a reference to the conversation.
+            AddOrUpdateContinuationParameters(dc.Context);
+
+            // Render message with continuation link.
+            await dc.Context.SendActivityAsync(MessageFactory.Text(string.Format(NotifyMessage, _serverUrl, dc.Context.Activity.From.Id)), cancellationToken);
             return EndOfTurn;
         }
 
-        public override async Task<DialogTurnResult> ContinueDialogAsync(DialogContext dc, CancellationToken cancellationToken = new CancellationToken())
+        public override async Task<DialogTurnResult> ContinueDialogAsync(DialogContext dc, CancellationToken cancellationToken = default)
         {
             if (dc.Context.Activity.Type == ActivityTypes.Event && dc.Context.Activity.Name == ActivityEventNames.ContinueConversation)
             {
+                // We continued the conversation, forget the proactive reference.
+                _continuationParametersStore.TryRemove(dc.Context.Activity.From.Id, out _);
+
                 // The continue conversation activity comes from the ProactiveController when the notification is received
                 await dc.Context.SendActivityAsync("We received a proactive message, ending the dialog", cancellationToken: cancellationToken);
 
@@ -41,9 +53,25 @@ namespace Microsoft.BotFrameworkFunctionalTests.WaterfallSkillBot.Dialogs.Proact
             }
 
             // Keep waiting for a call to the ProactiveController.
-            await dc.Context.SendActivityAsync($"We are waiting for a proactive message. {string.Format(NotifyMessage, _serverUrl, Guid.NewGuid())}", cancellationToken: cancellationToken);
+            await dc.Context.SendActivityAsync($"We are waiting for a proactive message. {string.Format(NotifyMessage, _serverUrl, dc.Context.Activity.From.Id)}", cancellationToken: cancellationToken);
 
             return EndOfTurn;
+        }
+
+        /// <summary>
+        /// Helper to extract and store parameters we need to continue a conversation from a proactive message.
+        /// </summary>
+        /// <param name="turnContext">A turnContext instance with the parameters we need.</param>
+        private void AddOrUpdateContinuationParameters(ITurnContext turnContext)
+        {
+            var continuationParameters = new ContinuationParameters
+            {
+                ClaimsIdentity = turnContext.TurnState.Get<IIdentity>(BotAdapter.BotIdentityKey),
+                ConversationReference = turnContext.Activity.GetConversationReference(),
+                OAuthScope = turnContext.TurnState.Get<string>(BotAdapter.OAuthScopeKey)
+            };
+
+            _continuationParametersStore.AddOrUpdate(turnContext.Activity.From.Id, continuationParameters, (key, newValue) => continuationParameters);
         }
     }
 }
