8765bcf45186028a481afe71b728e41bee27bda5
Santiago Grangetto <santiago.grangetto@southworks.com>
2021-02-12 12:43:17 -0300

    Add FileUpload test

---
+++ Tests/SkillFunctionalTests/FileUpload/FileUploadTests.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SkillFunctionalTests.Common;
+using TranscriptTestRunner;
+using TranscriptTestRunner.XUnit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SkillFunctionalTests.FileUpload
+{
+    [Trait("TestCategory", "FileUpload")]
+    public class FileUploadTests : ScriptTestBase
+    {
+        private readonly string _testScriptsFolder = Directory.GetCurrentDirectory() + @"/FileUpload/TestScripts";
+
+        public FileUploadTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        public static IEnumerable<object[]> TestCases()
+        {
+            var clientTypes = new List<ClientType> { ClientType.DirectLine };
+
+            var deliverModes = new List<string>
+            {
+                DeliveryModes.Normal,
+                DeliveryModes.ExpectReplies
+            };
+
+            var hostBots = new List<HostBot>
+            {
+                HostBot.WaterfallHostBotDotNet,
+
+                // TODO: Enable these when the ports to JS, Python and composer are ready
+                //HostBotNames.WaterfallHostBotJS,
+                //HostBotNames.WaterfallHostBotPython,
+                //HostBotNames.ComposerHostBotDotNet
+            };
+
+            var targetSkills = new List<string>
+            {
+                SkillBotNames.WaterfallSkillBotDotNet,
+
+                // TODO: Enable these when the ports to JS, Python and composer are ready
+                //SkillBotNames.WaterfallSkillBotJS,
+                //SkillBotNames.WaterfallSkillBotPython,
+                //SkillBotNames.ComposerSkillBotDotNet
+            };
+
+            var scripts = new List<string>
+            {
+                "FileUpload1.json",
+            };
+
+            var testCaseBuilder = new TestCaseBuilder();
+            var testCases = testCaseBuilder.BuildTestCases(clientTypes, deliverModes, hostBots, targetSkills, scripts);
+            foreach (var testCase in testCases)
+            {
+                yield return testCase;
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public async Task RunTestCases(TestCaseDataObject testData)
+        {
+            var testCase = testData.GetObject<TestCase>();
+            Logger.LogInformation(JsonConvert.SerializeObject(testCase, Formatting.Indented));
+
+            var options = TestClientOptions[testCase.HostBot];
+            var runner = new XUnitTestRunner(new TestClientFactory(testCase.ClientType, options, Logger).GetTestClient(), TestRequestTimeout, Logger);
+
+            // Execute the first part of the conversation.
+            var testParams = new Dictionary<string, string>
+            {
+                { "DeliveryMode", testCase.DeliveryMode },
+                { "TargetSkill", testCase.TargetSkill },
+                { "FileLocation", "\\\"C:\\\\local\\\\Temp\\\\architecture-resize.png\\\"" }
+            };
+            await runner.RunTestAsync(Path.Combine(_testScriptsFolder, testCase.Script), testParams);
+
+            // Create attachment to send.
+            var sendAttachment = new Attachment
+            {
+                Name = @"architecture-resize.png",
+                ContentType = "image/png",
+                ContentUrl = "https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png",
+            };
+
+            var attachments = new List<Attachment>();
+            attachments.Add(sendAttachment);
+
+            // Send FileUpload activity
+            var sendActivity = new Activity
+            {
+                Type = ActivityTypes.Message,
+                Attachments = attachments
+            };
+
+            await runner.SendActivityAsync(sendActivity);
+
+            // Execute the rest of the conversation.
+            await runner.RunTestAsync(Path.Combine(_testScriptsFolder, "FileUpload2.json"), testParams);
+        }
+    }
+}
