4f6ba0f6584f5e14fbd6154c342b238aed252da1
Gabo Gilabert <ggilaber@microsoft.com>
2021-02-19 10:05:45 -0500

    Added Group property in SkillDefintion and upated appsettings.json with the property so we can render skill groups dynamically (there are still some assumptions on their names in SkillsConfiguration) Updated maindialog with new wording for skill groups, special case logic for SSO and to skip prompting for actions for skills that only have one action (echo). Removed explicity ListStyle for some choice prompts so this is handled by the SDK defaults. Refactored SkillDefintion and derived classes to use enums and make the code less repetitive. Renamed TeamsSsoConnectionName to SsoConnectionNameTeams (it sorts better) Updated SsoSignInDialog to display the connection name being used. Some renames in SsoDialog to align with the SDK naming

--- Bots/DotNet/Consumers/CodeFirst/WaterfallHostBot/TokenExchangeSkillHandler.cs
+++ Bots/DotNet/Consumers/CodeFirst/WaterfallHostBot/TokenExchangeSkillHandler.cs
@@ -111,7 +111,7 @@ namespace Microsoft.BotFrameworkFunctionalTests.WaterfallHostBot
                             context.TurnState.Add<IIdentity>("BotIdentity", claimsIdentity);
 
                             // We need to know what connection name to use for the token exchange so we figure that out here
-                            var connectionName = targetSkill.Id == Constants.WaterfallSkillBotDotNet ? _configuration.GetSection("SsoConnectionName").Value : _configuration.GetSection("TeamsSsoConnectionName").Value;
+                            var connectionName = targetSkill.Id == Constants.WaterfallSkillBotDotNet ? _configuration.GetSection("SsoConnectionName").Value : _configuration.GetSection("SsoConnectionNameTeams").Value;
                             
                             if (string.IsNullOrEmpty(connectionName))
                             {
