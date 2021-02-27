14ee9d82da9f828e0b5c75aaf9824ff8cebe54c8
Denise Scollo <dens.scollo@gmail.com>
2021-02-27 13:41:05 -0300

    [Skills Everywhere] [#297] Remove ClientType enum and use SDK channel constants instead (#309)
    * Remove ClientType, replace with Channel constants
    
    * Rename clientType to channelId
    
    Co-authored-by: iscaltritti-sw <ian.scaltritti@southworks.com>

--- Tests/SkillFunctionalTests/SingleTurn/EchoTests.cs
+++ Tests/SkillFunctionalTests/SingleTurn/EchoTests.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -27,7 +28,7 @@ namespace SkillFunctionalTests.SingleTurn
 
         public static IEnumerable<object[]> TestCases()
         {
-            var clientTypes = new List<ClientType> { ClientType.DirectLine };
+            var channelIds = new List<string> { Channels.Directline };
             var deliverModes = new List<string>
             {
                 DeliveryModes.Normal,
@@ -74,7 +75,7 @@ namespace SkillFunctionalTests.SingleTurn
                 return false;
             }
 
-            var testCases = testCaseBuilder.BuildTestCases(clientTypes, deliverModes, hostBots, targetSkills, scripts, ShouldExclude);
+            var testCases = testCaseBuilder.BuildTestCases(channelIds, deliverModes, hostBots, targetSkills, scripts, ShouldExclude);
             foreach (var testCase in testCases)
             {
                 yield return testCase;
@@ -90,7 +91,7 @@ namespace SkillFunctionalTests.SingleTurn
 
             var options = TestClientOptions[testCase.HostBot];
 
-            var runner = new XUnitTestRunner(new TestClientFactory(testCase.ClientType, options, Logger).GetTestClient(), TestRequestTimeout, Logger);
+            var runner = new XUnitTestRunner(new TestClientFactory(testCase.ChannelId, options, Logger).GetTestClient(), TestRequestTimeout, Logger);
 
             var testParams = new Dictionary<string, string>
             {
