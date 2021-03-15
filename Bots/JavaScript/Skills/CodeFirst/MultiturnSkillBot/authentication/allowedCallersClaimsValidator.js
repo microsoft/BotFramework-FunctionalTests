31e8b1b6a2b727eb1548ccda044f4b083e82721f
Josh Gummersall <jgummersall@microsoft.com>
2021-03-15 14:23:14 -0700

    fix: lint --fix
    some manual fixes as well

--- Bots/JavaScript/Skills/CodeFirst/MultiturnSkillBot/authentication/allowedCallersClaimsValidator.js
+++ Bots/JavaScript/Skills/CodeFirst/MultiturnSkillBot/authentication/allowedCallersClaimsValidator.js
@@ -16,18 +16,18 @@ const allowedCallers = process.env.AllowedCallers ? process.env.AllowedCallers.s
  * @param claims An array of Claims decoded from the HTTP request's auth header.
  */
 const allowedCallersClaimsValidator = async (claims) => {
-    // For security, developer must specify allowedCallers.
-    if (!allowedCallers || allowedCallers.length === 0) {
-        throw new Error('AllowedCallers not specified in .env.');
+  // For security, developer must specify allowedCallers.
+  if (!allowedCallers || allowedCallers.length === 0) {
+    throw new Error('AllowedCallers not specified in .env.');
+  }
+  // If allowedCallers contains an '*', we allow all callers.
+  if (!allowedCallers.includes('*') && SkillValidation.isSkillClaim(claims)) {
+    // Check that the appId claim in the skill request is in the list of callers configured for this bot.
+    const appId = JwtTokenValidation.getAppIdFromClaims(claims);
+    if (!allowedCallers.includes(appId)) {
+      throw new Error(`Received a request from a bot with an app ID of "${appId}". To enable requests from this caller, add the app ID to your configuration file.`);
     }
-    // If allowedCallers contains an '*', we allow all callers.
-    if (!allowedCallers.includes('*') && SkillValidation.isSkillClaim(claims)) {
-        // Check that the appId claim in the skill request is in the list of callers configured for this bot.
-        const appId = JwtTokenValidation.getAppIdFromClaims(claims);
-        if (!allowedCallers.includes(appId)) {
-            throw new Error(`Received a request from a bot with an app ID of "${ appId }". To enable requests from this caller, add the app ID to your configuration file.`);
-        }
-    }
+  }
 };
 
 module.exports.allowedCallersClaimsValidator = allowedCallersClaimsValidator;
