1287485aebd4db9ba940bf0b966c6e201f355988
Santiago Grangetto <santiago.grangetto@southworks.com>
2021-04-08 17:03:14 -0300

    Add simple unit test for TranscriptTestRunner

---
+++ Tests/TranscriptTestRunnerTests/TestRunnerTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using TranscriptTestRunner;
+using Xunit;
+
+namespace TranscriptTestRunnerTests
+{
+    public class TestRunnerTests
+    {
+        [Fact]
+        public void CreateTestRunnerTest()
+        {
+            var testClientMock = new Mock<TestClientBase>();
+            var testRunner = new TestRunner(testClientMock.Object);
+
+            Assert.NotNull(testRunner);
+        }
+    }
+}
