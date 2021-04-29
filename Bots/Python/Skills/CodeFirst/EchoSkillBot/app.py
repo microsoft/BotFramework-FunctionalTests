8abff729c41f849131926eaf6b2706bd7b56c3aa
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2021-04-29 16:19:46 -0300

    Update error handler output for python bots (#399)
    Co-authored-by: Giuliano Taliano <54330145+GiulianoDolceTalianoSantoro@users.noreply.github.com>
    Co-authored-by: Gabo Gilabert <gabog@users.noreply.github.com>

--- Bots/Python/Skills/CodeFirst/EchoSkillBot/app.py
+++ Bots/Python/Skills/CodeFirst/EchoSkillBot/app.py
@@ -11,9 +11,10 @@ from aiohttp.web import Request, Response
 from botbuilder.core import (
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
-    TurnContext
+    TurnContext,
+    MessageFactory,
 )
-from botbuilder.schema import Activity, ActivityTypes
+from botbuilder.schema import Activity, ActivityTypes, InputHints
 from botframework.connector.auth import AuthenticationConfiguration
 
 from bots import EchoBot
@@ -43,31 +44,52 @@ async def on_error(context: TurnContext, error: Exception):
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
     traceback.print_exc()
 
-    # Send a message to the user
-    await context.send_activity("The skill encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
-    )
+    try:
+        exc_info = sys.exc_info()
+        stack = traceback.format_exception(*exc_info)
 
-    # Send a trace activity, which will be displayed in Bot Framework Emulator
-    if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error",
+        # Send a message to the user
+        error_message_text = "The skill encountered an error or bug."
+        error_message = MessageFactory.text(
+            f"{error_message_text}\r\n{error}\r\n{stack}",
+            error_message_text,
+            InputHints.ignoring_input,
         )
-        await context.send_activity(trace_activity)
+        error_message.value = {"message": error, "stack": stack}
+        await context.send_activity(error_message)
 
-    # Send and EndOfConversation activity to the skill caller with the error to end the conversation and let the
-    # caller decide what to do. Send a trace activity if we're talking to the Bot Framework Emulator
-    end_of_conversation = Activity(
-        type=ActivityTypes.end_of_conversation, code="SkillError", text=f"{error}"
-    )
-    await context.send_activity(end_of_conversation)
+        error_message_text = (
+            "To continue to run this bot, please fix the bot source code."
+        )
+        error_message = MessageFactory.text(
+            error_message_text, error_message_text, InputHints.expecting_input
+        )
+        await context.send_activity(error_message)
+
+        # Send a trace activity, which will be displayed in Bot Framework Emulator
+        if context.activity.channel_id == "emulator":
+            # Create a trace activity that contains the error object
+            trace_activity = Activity(
+                label="TurnError",
+                name="on_turn_error Trace",
+                timestamp=datetime.utcnow(),
+                type=ActivityTypes.trace,
+                value=f"{error}",
+                value_type="https://www.botframework.com/schemas/error",
+            )
+            await context.send_activity(trace_activity)
+
+        # Send and EndOfConversation activity to the skill caller with the error to end the conversation and let the
+        # caller decide what to do. Send a trace activity if we're talking to the Bot Framework Emulator
+        end_of_conversation = Activity(
+            type=ActivityTypes.end_of_conversation, code="SkillError", text=f"{error}"
+        )
+        await context.send_activity(end_of_conversation)
+    except Exception as exception:
+        print(
+            f"\n Exception caught on on_error : {exception}", file=sys.stderr,
+        )
+        traceback.print_exc()
 
 
 ADAPTER.on_turn_error = on_error
@@ -89,10 +111,10 @@ async def messages(req: Request) -> Response:
     try:
         response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
         # DeliveryMode => Expected Replies
-        if (response):
+        if response:
             body = json.dumps(response.body)
             return Response(status=response.status, body=body)
-        
+
         # DeliveryMode => Normal
         return Response(status=HTTPStatus.CREATED)
     except Exception as exception:
