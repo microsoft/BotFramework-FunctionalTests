4b27253b6e2d605e4ea9e1eb0fbca60616b6cc57
Santiago Grangetto <santiago.grangetto@southworks.com>
2021-07-14 12:04:39 -0300

    [R13] Update BotBuilder version to latest 4.13.x in all projects (#469)
    * Revert EchoSkillBot-JS CloudAdapter implementation
    
    * Update all bots to latest 4.13.x
    
    * Update TranscriptConverter and TestRunner BB versions

--- Bots/JavaScript/Skills/CodeFirst/EchoSkillBot/bot.js
+++ Bots/JavaScript/Skills/CodeFirst/EchoSkillBot/bot.js
@@ -1,16 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const {
-  ActivityHandler,
-  ActivityTypes,
-  EndOfConversationCodes
-} = require('botbuilder');
+const { ActivityHandler, ActivityTypes, EndOfConversationCodes } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
   constructor () {
     super();
-
     // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
     this.onMessage(async (context, next) => {
       const activityText = context.activity.text.toLowerCase();
@@ -22,16 +17,14 @@ class EchoBot extends ActivityHandler {
         });
       } else {
         await context.sendActivity(`Echo: ${context.activity.text}`);
-        await context.sendActivity(
-          'Say "end" or "stop" and I\'ll end the conversation and back to the parent.'
-        );
+        await context.sendActivity('Say "end" or "stop" and I\'ll end the conversation and back to the parent.');
       }
 
       // By calling next() you ensure that the next BotHandler is run.
       await next();
     });
 
-    this.onUnrecognizedActivityType(async (_context, next) => {
+    this.onUnrecognizedActivityType(async (context, next) => {
       // This will be called if the root bot is ending the conversation.  Sending additional messages should be
       // avoided as the conversation may have been deleted.
       // Perform cleanup of resources if needed.
@@ -42,4 +35,4 @@ class EchoBot extends ActivityHandler {
   }
 }
 
-module.exports = { EchoBot };
+module.exports.EchoBot = EchoBot;
