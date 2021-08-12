c1f294aaa8a174ae7c85019813a512ba8f42a00c
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2021-08-12 11:16:10 -0300

    [#358] Port .Net Skill bots to Cloud Adapter - WaterfallSkillBot (#477)
    * Port .NET WaterfallSkillBot to Cloud Adapter
    
    * Fix AuthDialog issue
    
    * Update how the BotFrameworkAuthentication is assigned in the Startup for WaterfallSkillBotDotNet
    
    Co-authored-by: sw-joelmut <joel.mut@southworks.com>

--- Bots/DotNet/Skills/CodeFirst/WaterfallSkillBot/Dialogs/Auth/AuthDialog.cs
+++ Bots/DotNet/Skills/CodeFirst/WaterfallSkillBot/Dialogs/Auth/AuthDialog.cs
@@ -51,13 +51,13 @@ namespace Microsoft.BotFrameworkFunctionalTests.WaterfallSkillBot.Dialogs.Auth
                 stepContext.Values["Token"] = tokenResponse.Token;
 
                 // Show the token
-                var loggedInMessage = "You are now logged in.";
+                const string loggedInMessage = "You are now logged in.";
                 await stepContext.Context.SendActivityAsync(MessageFactory.Text(loggedInMessage, loggedInMessage, InputHints.IgnoringInput), cancellationToken);
 
                 return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text("Would you like to view your token?") }, cancellationToken);
             }
 
-            var tryAgainMessage = "Login was not successful please try again.";
+            const string tryAgainMessage = "Login was not successful please try again.";
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(tryAgainMessage, tryAgainMessage, InputHints.IgnoringInput), cancellationToken);
             return await stepContext.ReplaceDialogAsync(InitialDialogId, cancellationToken: cancellationToken);
         }
@@ -67,14 +67,23 @@ namespace Microsoft.BotFrameworkFunctionalTests.WaterfallSkillBot.Dialogs.Auth
             var result = (bool)stepContext.Result;
             if (result)
             {
-                var showTokenMessage = "Here is your token:";
+                const string showTokenMessage = "Here is your token:";
                 await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{showTokenMessage} {stepContext.Values["Token"]}", showTokenMessage, InputHints.IgnoringInput), cancellationToken);
             }
 
             // Sign out
-            var botAdapter = (BotFrameworkAdapter)stepContext.Context.Adapter;
-            await botAdapter.SignOutUserAsync(stepContext.Context, _connectionName, null, cancellationToken);
-            var signOutMessage = "I have signed you out.";
+            const string signOutMessage = "I have signed you out.";
+            var oauthPrompt = new OAuthPrompt(
+                "SignOut",
+                new OAuthPromptSettings
+                {
+                    ConnectionName = _connectionName,
+                    Text = signOutMessage,
+                    Title = "Sign Out"
+                });
+
+            await oauthPrompt.SignOutUserAsync(stepContext.Context, cancellationToken);
+
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(signOutMessage, signOutMessage, inputHint: InputHints.IgnoringInput), cancellationToken);
 
             return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
