f66416863e6c6fcdb86cd5af41a9afe17977d894
Giuliano Taliano <54330145+GiulianoDolceTalianoSantoro@users.noreply.github.com>
2021-08-12 15:51:06 -0300

    Add FileUpload scenario with "Download File" custom action

---
+++ Bots/DotNet/Skills/Composer/ComposerSkillBotDotNet/DownloadFileDialog/Action/DownloadFileDialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace DownloadFileDialog.Action
+{
+    public class DownloadFileDialog : Dialog
+    {
+        [JsonProperty("$kind")]
+        public const string Kind = "DownloadFileDialog";
+
+        [JsonConstructor]
+        public DownloadFileDialog([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+            : base()
+        {
+            // enable instances of this command as debug break point
+            RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        [JsonProperty("file")]
+        public ObjectExpression<Attachment> File { get; set; }
+
+        [JsonProperty("resultProperty")]
+        public StringExpression ResultProperty { get; set; }
+
+        public async override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
+        {
+            var file = File.GetValue(dc.State);
+
+            var remoteFileUrl = file.ContentUrl;
+            var localFileName = Path.Combine(Path.GetTempPath(), file.Name);
+            string fileContent;
+
+            using (var webClient = new WebClient())
+            {
+                webClient.DownloadFile(remoteFileUrl, localFileName);
+                using var reader = new StreamReader(localFileName);
+                fileContent = await reader.ReadToEndAsync();
+            }
+
+            if (this.ResultProperty != null)
+            {
+                dc.State.SetValue(this.ResultProperty.GetValue(dc.State), fileContent);
+            }
+
+            return await dc.EndDialogAsync(result: fileContent, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ Bots/DotNet/Skills/Composer/ComposerSkillBotDotNet/DownloadFileDialog/DownloadFileDialogBotComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DownloadFileDialog
+{
+    public class DownloadFileDialogBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            // Anything that could be done in Startup.ConfigureServices can be done here.
+            // In this case, the DownloadFileDialog needs to be added as a new DeclarativeType.
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<Action.DownloadFileDialog>(Action.DownloadFileDialog.Kind));
+        }
+    }
+}
