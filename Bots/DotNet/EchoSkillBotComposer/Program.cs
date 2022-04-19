6e76c85ea626aca6fb29ba273df6106a779527a5
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2022-04-19 14:49:15 -0300

    Update namespaces in composer bots (#569)

--- Bots/DotNet/EchoSkillBotComposer/Program.cs
+++ Bots/DotNet/EchoSkillBotComposer/Program.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Runtime.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-namespace EchoSkillBotComposer
+namespace Microsoft.Bot.Builder.FunctionalTestsBots.EchoSkillBotComposer
 {
     public class Program
     {
