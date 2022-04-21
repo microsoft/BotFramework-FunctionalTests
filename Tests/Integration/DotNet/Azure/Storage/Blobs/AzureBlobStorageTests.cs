8aa5a143a367b0411b0f4342222538ca12863f95
sw-joelmut <joel.mut@southworks.com>
2022-04-21 14:59:16 -0300

    Add AzureBlobs

---
+++ Tests/Integration/DotNet/Azure/Storage/Blobs/AzureBlobStorageTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Tests.Integration.Azure.Storage.Blobs
+{
+    [Trait("TestCategory", "Deprecated")]
+    public class AzureBlobStorageTests : StorageBaseTests, IClassFixture<AzureBlobStorageFixture>
+    {
+        public AzureBlobStorageTests(AzureBlobStorageFixture azureBlobFixture)
+        {
+            UseStorages(azureBlobFixture.Storages);
+        }
+    }
+}
