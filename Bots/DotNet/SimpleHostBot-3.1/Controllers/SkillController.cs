2c62aaa5319b46ca788bde746c6e2dda8a9f0f95
CeciliaAvila <cecilia.avila@southworks.com>
2022-05-16 10:51:34 -0300

    Add NetCore 3.1 bots

---
+++ Bots/DotNet/SimpleHostBot-3.1/Controllers/SkillController.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Skills;
+
+namespace Microsoft.Bot.Builder.FunctionalTestsBots.SimpleHostBot31.Controllers
+{
+    /// <summary>
+    /// A controller that handles skill replies to the bot.
+    /// This example uses the <see cref="CloudSkillHandler"/> that is registered as a <see cref="ChannelServiceHandlerBase"/> in startup.cs.
+    /// </summary>
+    [ApiController]
+    [Route("api/skills")]
+    public class SkillController : ChannelServiceController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkillController"/> class.
+        /// </summary>
+        /// <param name="handler">The skill handler registered as ChannelServiceHandlerBase.</param>
+        public SkillController(ChannelServiceHandlerBase handler)
+            : base(handler)
+        {
+        }
+    }
+}
