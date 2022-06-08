a1daeb80ac39c15ae18818d9bca5fc9920da5aa7
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2022-06-08 14:41:08 -0300

    [#562][DotNet] Update existing Cosmos tests (#584)
    * Add base/shared files and update namespace
    
    * Update CosmosDb to use StorageBaseTests
    
    Co-authored-by: sw-joelmut <joel.mut@southworks.com>

--- Tests/Integration/DotNet/Azure/Cosmos/CosmosDbStorageFixture.cs
+++ Tests/Integration/DotNet/Azure/Cosmos/CosmosDbStorageFixture.cs
@@ -2,12 +2,14 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Tests.Integration.Azure.Storage;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Microsoft.Bot.Builder.Tests.Integration.Azure.Cosmos
@@ -20,21 +22,27 @@ namespace Microsoft.Bot.Builder.Tests.Integration.Azure.Cosmos
             PartitionedContainerId = "CosmosPartitionedContainer";
         }
 
-        public IStorage Storage { get; private set; }
-
         public string PartitionedContainerId { get; private set; }
+
+        public IDictionary<StorageCase, IStorage> Storages { get; private set; }
 
         public new async Task InitializeAsync()
         {
             await base.InitializeAsync();
 
-            Storage = new CosmosDbStorage(new CosmosDbStorageOptions
+            var options = new CosmosDbStorageOptions
             {
                 AuthKey = AuthKey,
                 CollectionId = ContainerId,
                 CosmosDBEndpoint = new Uri(ServiceEndpoint),
                 DatabaseId = DatabaseId,
-            });
+            };
+
+            Storages = new Dictionary<StorageCase, IStorage>
+            {
+                { StorageCase.Default, new CosmosDbStorage(options) },
+                { StorageCase.TypeNameHandlingNone, new CosmosDbStorage(options, new JsonSerializer() { TypeNameHandling = TypeNameHandling.None }) }
+            };
         }
 
         public IStorage GetStoragePartitionedContainer(string partitionKey)
